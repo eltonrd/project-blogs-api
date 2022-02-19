323c3e7529549db281f55d55ef14a48a58813191
Elton Rodrigues Dantas <el_dantas1999@hotmail.com>
2022-02-19 18:12:26 -0300

    Correção da migration

--- migrations/20220219205429-create-user.js
+++ migrations/20220219205429-create-user.js
@@ -24,14 +24,6 @@ module.exports = {
       image: {
         type: Sequelize.STRING,
         allowNull: false,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
       }
     });
   },
